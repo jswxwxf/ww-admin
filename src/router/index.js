2c852cd82db5a581319f4ed2f1e32bc437273874
jeffreywang <wxwxf@hotmail.com>
2024-09-25 11:04:35 +0800

    add element plus table page

--- src/router/index.js
+++ src/router/index.js
@@ -94,36 +94,37 @@ export const publicRoutes = [
       },
     ],
   },
-  // {
-  //   path: '/table',
-  //   component: Layouts,
-  //   redirect: '/table/element-plus',
-  //   name: 'Table',
-  //   meta: {
-  //     title: '表格',
-  //     elIcon: 'Grid',
-  //   },
-  //   children: [
-  //     {
-  //       path: 'element-plus',
-  //       component: () => import('@/views/table/element-plus/index.vue'),
-  //       name: 'ElementPlus',
-  //       meta: {
-  //         title: 'Element Plus',
-  //         keepAlive: true,
-  //       },
-  //     },
-  //     {
-  //       path: 'vxe-table',
-  //       component: () => import('@/views/table/vxe-table/index.vue'),
-  //       name: 'VxeTable',
-  //       meta: {
-  //         title: 'Vxe Table',
-  //         keepAlive: true,
-  //       },
-  //     },
-  //   ],
-  // },
+  {
+    path: '/table',
+    component: Layouts,
+    redirect: '/table/element-plus',
+    name: 'Table',
+    meta: {
+      title: '表格',
+      elIcon: 'Grid',
+      alwaysShow: true,
+    },
+    children: [
+      {
+        path: 'element-plus',
+        component: () => import('@/views/table/element-plus/index.vue'),
+        name: 'ElementPlus',
+        meta: {
+          title: 'Element Plus',
+          keepAlive: true,
+        },
+      },
+      // {
+      //   path: 'vxe-table',
+      //   component: () => import('@/views/table/vxe-table/index.vue'),
+      //   name: 'VxeTable',
+      //   meta: {
+      //     title: 'Vxe Table',
+      //     keepAlive: true,
+      //   },
+      // },
+    ],
+  },
   {
     path: '/menu',
     component: Layouts,
